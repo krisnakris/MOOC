239e38b081a9f4052a3bf4b2b329b0d96b43013a
krisnakris <krisna.post@gmail.com>
2021-01-22 20:37:11 +0700

    PhoneBookDone

--- part2/info/src/App.js
+++ part2/info/src/App.js
@@ -1,12 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
+
+const Filter = (props) => {
+return (
+    <div>
+      filter shown with <input value={props.value} onChange={props.onChange}/>
+    </div>
+  )
+}
+
+const PersonForm = (props) => {
+  return (
+    <form onSubmit={props.onSubmit}>
+    <div>
+      name: <input value={props.valuePerson} onChange={props.onChangePerson}/>
+    </div>
+    <div>
+      number: <input value={props.valueNumber} onChange={props.onChangeNumber}/>
+    </div>
+    <div>
+      <button type={props.type}>add</button>
+    </div>
+  </form>
+  )
+}
+
+const Persons = (props) => {
+  return (
+    <div>
+    {props.name.map(name =>
+      <div key={name.id}> {name.name} {name.number}</div>
+    )}
+  </div>
+  )
+}
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
+
+  useEffect(() => {
+  console.log('effect');
+  axios.get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promised fulfilled');
+      setPersons(response.data);
+    })
+  },[])
 
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber] = useState('');
@@ -27,7 +66,7 @@ const App = () => {
     if (checkDuplicate()) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
-      const personObject = {
+        const personObject = {
         name : newName,
         number : newNumber
       }
@@ -56,27 +95,19 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={showAll} onChange={handleFilterChange}/>
-      </div>
+      <Filter value = {showAll} onChange= {handleFilterChange}/>
+
       <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handlePersonChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handlePersonNumber}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+
+      <PersonForm onSubmit = {addPerson} valuePerson = {newName} onChangePerson 
+       = {handlePersonChange} valueNumber = {newNumber} onChangeNumber = {handlePersonNumber}
+      type= 'submit'
+      />
+
       <h2>Numbers</h2>
-      <div>
-        {namesToShow.map(name =>
-          <div> {name.name} {name.number}</div>
-        )}
-      </div>
+
+      <Persons name = {namesToShow}/>
+
     </div>
   )
 }
