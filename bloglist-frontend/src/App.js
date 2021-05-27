c38fe30267c850fe1147f9c6e077ea364a4adebe
krisnakris <krisna.post@gmail.com>
2021-05-27 09:24:15 +0700

    part 5.1-5.4 done

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,22 +1,170 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [status, setStatus] = useState(null);
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [url, setUrl] = useState('');
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      window.localStorage.setItem('loggedBlogUser', JSON.stringify(user));
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      setErrorMessage('Wrong credentials');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  }
+
+  const handleLogout = (event) => {
+    event.preventDefault();
+    setUser(null);
+    blogService.setToken(null);
+    window.localStorage.clear();
+  }
+
+  const addBlog = (event) => {
+    event.preventDefault();
+    const blogObject = {
+      title, author, url
+    };
+
+    blogService
+      .create(blogObject)
+      .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setAuthor('')
+        setUrl('')
+        setTitle('')
+        setStatus('success')
+        setErrorMessage(`${blogObject.title} by ${blogObject.author} added`);
+        setTimeout(() => {
+          setErrorMessage(null);
+          setStatus(null);
+        }, 5000);
+      });
+  };
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  }
+
+  const handleAuthorChange = (event) => {
+    setAuthor(event.target.value);
+  }
+
+  const handleUrlChange = (event) => {
+    setUrl(event.target.value);
+  }
+
+  const blogForm = () => (
+    <>
+      <h2>Create new Blog</h2>
+      <form onSubmit={addBlog}>
+        <div>
+          title:
+          <input
+            value={title}
+            onChange={handleTitleChange}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            value={author}
+            onChange={handleAuthorChange}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            value={url}
+            onChange={handleUrlChange}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </>
+  )
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <h2>Blogs</h2>
+      <Notification message={errorMessage} status={status} />
+
+      { user === null ?
+        loginForm() :
+        <div>
+          <p> {user.name} logged-in <button onClick={handleLogout}>logout</button></p>
+          <div>{blogForm()} </div>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+      }
+
+
     </div>
   )
 }
